01bd966e8b79d42809297751ae4ac3bfbd6d7b3c
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-03-21 19:40:51 -0400

    Serve the React notes frontend from the backend.

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ let notes = [
   },
 ];
 
+app.use(express.static("dist"));
+
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
